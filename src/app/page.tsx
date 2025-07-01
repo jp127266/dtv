54245a72834771ecd2aa4829ef3194fadef75e6c
jp127266 <jp127266@gmail.com>
2025-07-01 08:42:01 +0000

    ... baselining before adding Google Authentication.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,40 +2,40 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
-        <h1 className="text-2xl text-center sm:text-3xl">Next.js</h1>
+        <h1 className="text-2xl text-center sm:text-3xl">Welcome to DigiCAP.tv</h1>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://docs.sevalla.com/application-hosting"
+          href="https://www.notion.so/DigiCaster-Wiki-4bb9e5aa7d044086b4d186d18f226e69?source=copy_link"
           target="_blank"
           rel="noopener noreferrer"
         >
-          📁 Read Sevalla docs
+          📁 Read DigiCaster docs
         </a>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.notion.so/16e3ad50cbcd41d1bbf069f43e972498?v=61c45b66c52040f9bea8bfc74833dca5&source=copy_link"
           target="_blank"
           rel="noopener noreferrer"
         >
-          📁 Read Next.js docs
+          📁 Read other product docs
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://atsc.digicap.tv/releases/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          📚 Learn
+          📚 Releases 
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="http://digicaps.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          🌐 Go to nextjs.org →
+          🌐 Go to DigiCAP HQ site →
         </a>
       </footer>
     </div>
